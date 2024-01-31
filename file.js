71572a1842b4932581dd3e64dde2f4758bb64896
Adrien Yakite <adrien.yakite@gmail.com>
2024-01-31 12:05:34 +0100

    Ajout file.js qui gère le form de contact Form de contact - Fonctionnel à 50% Ajout de style Ajout des média queries minimales

---
+++ file.js
@@ -0,0 +1,30 @@
+// gestion du form de contact
+
+const btnEnvoyer = document.querySelector("button");
+const zoneEmail = document.getElementById("mailUser");
+const zoneNom = document.getElementById("nomUser");
+const zoneMsg = document.getElementById("msgUser");
+let zoneForm = document.querySelector(".formContact");
+
+btnEnvoyer.addEventListener("click", (event)=>{
+    const emailUser = zoneEmail.value;
+    console.log(emailUser);
+    
+    const nomUser = zoneNom.value;
+    console.log(nomUser);
+
+    const msgUser = zoneMsg.value;
+    console.log(msgUser);
+    
+    //envoie du mail selon les infos récoltées
+     
+    
+    
+    
+    //affichage message sur la page
+    zoneForm.innerHTML=``;
+    zoneForm.classList.add("msgFinal");
+    zoneForm.innerText=`Merci pour votre message `  + nomUser + `, je vous recontacterai au plus vite !`  ;
+
+    event.preventDefault();
+})
